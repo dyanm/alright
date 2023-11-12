360d05300f7a64964564ad9f7b442e5c3f07fe21
dyanm <3048253+dyanm@users.noreply.github.com>
2023-11-12 09:03:28 +0800

    Update __init__.py

--- alright/__init__.py
+++ alright/__init__.py
@@ -189,6 +189,16 @@ class WhatsApp(object):
             # LOGGER.exception(f'It was not possible to fetch chat "{username}" because of e : {e}')
             return False
 
+    def wait_for_search_box(self):
+        try:
+            search_box = self.wait.until(
+                EC.presence_of_element_located(
+                    (By.XPATH, '//*[@id="side"]/div[1]/div/label/div/div[2]')
+                )
+            )
+        except Exception as bug:
+            LOGGER.exception(f"Exception raised while waiting for initialsation\n{bug}")
+    
     def username_exists(self, username):
         """username_exists ()
 
